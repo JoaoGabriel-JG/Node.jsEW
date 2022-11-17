e22b8bf98ceac9838ff4083caf03bcba10b62345
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-17 19:34:05 -0300

    att

--- lists/for-forin/1.for-forin-forof.js
+++ lists/for-forin/1.for-forin-forof.js
@@ -1,6 +1,6 @@
-const service = require('./service')    // Importar modulos criados por mim
+const service = require('./service')    // Importar modulos criados por mim \\
 
-async function main() {     // Vai manipular Promise? USA O ASYNC
+async function main() {     // Vai manipular Promise? USA O ASYNC \\
     try {
         const result = await service.obterPessoas('a')
         const names = []
@@ -11,6 +11,24 @@ async function main() {     // Vai manipular Promise? USA O ASYNC
             names.push(pessoa.name)
         }
         console.timeEnd('for')
+
+        // Nesse caso a melhor opção de velocidade foi o FOR IN \\
+        // Forin cria automaticamente o incrementador em cima do resultado utilizando menos linhas \\
+
+        console.time('forin')
+        for(let i in result.results) {
+            const pessoa = result.results[i]
+            names.push(pessoa.name)
+        }
+        console.timeEnd('forin')
+
+        // For of utiliza muitas poucas linhas \\
+
+        console.time('forof')
+        for(pessoa of result.results) {
+            names.push(pessoa.name)
+        }
+        console.timeEnd('forof')
 
         console.log(`names `, names)
     }
