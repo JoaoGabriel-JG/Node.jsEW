3440d9d96b65bb24abfaca0f7863715ac591e98c
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-17 19:17:51 -0300

    att

--- lists/for-forin/service.js
+++ lists/for-forin/service.js
@@ -1,16 +1,16 @@
 const axios = require('axios')
-const URL = `https://swapi.co/api/people`
+const URL = `https://swapi.dev/api/people`
 
-async function obterPessoas (nome) {    // Para manipular Promise
+async function obterPessoas (nome) {    // async: manipular Promises internamente da função
     const url = `${URL}/?search=${nome}&format=json`
     const response = await axios.get(url)
     return response.data
-}      
+}
 
 obterPessoas('r2')
     .then(function (resultado) {
-        console.log('resultado ', resultado)
+        console.log('Resultado: ', resultado)
     })
     .catch(function (error) {
-        console.error('DEU RUIM', error)
+        console.error('DEU RUIM ', error)
     })
