b429c5c9c266b7609802c0c62935b84a3ef3708c
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-17 19:57:12 -0300

    att

--- lists/for-forin/2.map.js
+++ lists/for-forin/2.map.js
@@ -1,4 +1,17 @@
 const service = require('./service')
+
+Array.prototype.meuMap = function (callback) {  // Substituir uma função global / metodo global de listas \\
+    const novoArrayMapeado = []
+    for (let indice = 0; indice <= this.length -1; indice++) {
+        const resultado = callback(this[indice], indice)    // Quando executar essas funções adiciona os parametros na lista \\
+        novoArrayMapeado.push(resultado)
+    }
+
+    return novoArrayMapeado
+} 
+
+// Mappode ser um for reduzido \\
+
 async function main () {
     try {
         const results = await service.obterPessoas(`a`)
@@ -11,8 +24,11 @@ async function main () {
         // })
 
         // Forma mais elegante \\        
-        const names = results.results.map( (pessoa) => pessoa.name )
+        // const names = results.results.map( (pessoa) => pessoa.name )
 
+        const names = results.results.meuMap(function (pessoa, indice) {
+            return `[ ${indice} ] ${pessoa.name}`
+        })
         console.log('names: ', names)
     }
     catch (error){
