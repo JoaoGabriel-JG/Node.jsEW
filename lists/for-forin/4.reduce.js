de37b07ef441d1a1015d34e77965774855c2c26a
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-21 17:38:54 -0300

    att

--- lists/for-forin/4.reduce.js
+++ lists/for-forin/4.reduce.js
@@ -3,9 +3,14 @@ const { obterPessoas } = require('./service')
 async function main() {
     try {
         const { results } = await obterPessoas('a')
-        const pesos = results.map(item => parseInt(item.heigth))
+        const pesos = results.map(item => parseInt(item.height))
+        console.log('Pesos: ', pesos)
         // [20.2, 30.3, 40.5 ] = X \\ somar todos os resultados para saber o valor final 
+        const total = pesos.reduce((anterior, proximo) => {
+            return anterior + proximo
+        })
 
+        console.log('Total: ', total)
         
     }
     catch (error) {
