140c9764bc4088a1067de4465704de7fd387fe28
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-25 19:18:01 -0300

    att

---
+++ testes/01/service.js
@@ -0,0 +1,14 @@
+const { get } = require('axios')
+
+const URL = `https://swapi.dev/api/people`
+
+async function obterPessoas(nome) {
+    const url = `${URL}/?search=${nome}&format=json`
+    const result = await get(url)
+
+    return result.data
+}
+
+module.exports = {
+    obterPessoas
+}
