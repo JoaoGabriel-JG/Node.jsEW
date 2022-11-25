dc3b5095612513e17bb85723c13336606c52099f
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-25 19:25:41 -0300

    att

--- testes/01/service.js
+++ testes/01/service.js
@@ -5,8 +5,14 @@ const URL = `https://swapi.dev/api/people`
 async function obterPessoas(nome) {
     const url = `${URL}/?search=${nome}&format=json`
     const result = await get(url)
+    return result.data.results.map(mapearPessoas)
+}
 
-    return result.data
+function mapearPessoas (item) {
+    return {
+        nome: item.name,
+        peso: item.height
+    }
 }
 
 module.exports = {
--- testes/01/tests.js
+++ testes/01/tests.js
@@ -4,7 +4,8 @@ const { obterPessoas } = require('./service')
 describe('Star Wars Testes', function () {
     it('deve buscar o r2d2 com o formato correto', async () => {
         const expected = [{ 
-            nome: 'R2-D2', peso: '96' 
+            nome: 'R2-D2', 
+            peso: '96' 
         }]
         const nomeBase = `r2-d2`
         const resultado = await obterPessoas(nomeBase)
