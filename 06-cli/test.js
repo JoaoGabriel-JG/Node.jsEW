09abe766e6f8216b16276e698e14e3caffbeef5a
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-26 15:38:43 -0300

    att

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -11,8 +11,10 @@ const DEFAULT_ITEM_CADASTRAR = {
 describe('Suite de manipulação de Herois', () => {  // Suite de teste 
     it('Deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
-        const resultado = await database.listar(expected.id)
-        ok(null, expected)
+        const [ resultado ] = await database.listar(expected.id)    // Como pegar só a primeira posição do Array DESTRUCTURING []
+        const posicaoUm = resultado[0]    // Como pegar só a primeira posição do Array
+
+        deepEqual(resultado, expected)      // deepEqual: completamente igual
     })
 
     // it('Deve cadastrar um heroi, usando arquivos', async () => {
