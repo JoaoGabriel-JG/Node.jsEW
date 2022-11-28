4bcecb3028287a4861560bcf627b45d2507506e5
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-11-28 18:56:01 -0300

    att

---
+++ 06-cli/index.js
@@ -0,0 +1,23 @@
+const Commander = require('commander')
+const dataBase = require('./database')
+
+async function main() {
+    Commander
+        .version('v1')
+        .option('-n, --nome [value]', "Nome do Heroi")
+        .option('-p, --poder [value]', "Poder do Heroi")
+
+        .option('-c, --cadastrar', "Cadastrar um Heroi")
+        .parse(process.argv)
+    try {
+        if (Commander.cadastrar) {
+            console.log(Commander)
+            // const resultado = await dataBase.cadastrar(Commander)
+        }
+    }
+    catch (error) {
+        console.error('DEU RUIM ', error)
+    }
+}
+
+main()
