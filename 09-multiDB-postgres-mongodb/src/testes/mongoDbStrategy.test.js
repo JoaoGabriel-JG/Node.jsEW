63735f33ec8afee4579d3396dfdd34f55c3202ef
João Gabriel <joao.nitsus@gmail.com>
2023-03-27 20:08:17 -0300

    att

--- 09-multiDB-postgres-mongodb/src/testes/mongoDbStrategy.test.js
+++ 09-multiDB-postgres-mongodb/src/testes/mongoDbStrategy.test.js
@@ -2,15 +2,23 @@ const assert = require('assert')
 const MongoDb = require('./../db/strategies/mongodb')
 const Context = require('./../db/strategies/base/contextStrategy')
 
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Nakahara Chuuya',
+    poder: 'Pelo Corrupto Pesar'
+}
 const context = new Context(new MongoDb())
 describe('MongoDB Suite de Testes', function () {
     this.beforeAll(async () => {
         await context.connect()
     })
-    it.only('Verificar conexão ', async () => {
+    it('Verificar conexão ', async () => {
         const result = await context.isConnected()
         const expected = 'Conectado'
 
         assert.deepEqual(result, expected)
+    })
+    it('cadastrar', async () => {
+        const { nome, poder } = context.create(MOCK_HEROI_CADASTRAR)
+        assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
     })
 })
